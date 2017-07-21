afc550c00c342825fa5a03c28729a4baa86df215
Jess Egler <jess.egler@gmail.com>
2017-07-21 13:51:42 -0500

    adds tests
    Change-Id: I9ecf35632e83d8c730ebc491c4ece4a006709d9d

--- keystoneclient/tests/unit/v3/test_projects.py
+++ keystoneclient/tests/unit/v3/test_projects.py
@@ -318,7 +318,21 @@ class ProjectTests(utils.ClientTestCase, utils.CrudTests):
         ref['tags'] = []
         tag_name = "blue"
 
-        url = stub_url("PUT",
-            parts=[self.collection_key, ref['id'], "tags", tag_name])
-
+        stub_url("PUT",
+            parts=[self.collection_key, ref['id'], "tags", tag_name],
+            status_code=201)
         self.manager.add_tag(ref['id'], tag_name)
+
+
+    def test_update_tags_replaces_existing(self):
+        new_tags = ["blue", "orange"]
+        ref = self.new_ref()
+        ref['tags'] = old_tags
+
+        stub_url("PUT",
+            parts=[self.collection_key, ref['id'], "tags"],
+            body=new_tags,
+            status_code=200)
+        
+        self.manager.update_tags(ref['id'], new_tags)
+
